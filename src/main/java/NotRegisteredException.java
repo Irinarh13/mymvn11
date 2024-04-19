303341fdc1329395daa417de3c19f77e1aca07dc
Irinarh <jagdtiger_86@mail.ru>
2024-04-19 01:54:12 -0700

    Testing systems that manage a set of objects commit

---
+++ src/main/java/NotRegisteredException.java
@@ -0,0 +1,8 @@
+
+
+public class NotRegisteredException extends RuntimeException {
+    public NotRegisteredException(String name) {
+
+        super("Игрок с именем" + name + "не найден");
+    }
+}
